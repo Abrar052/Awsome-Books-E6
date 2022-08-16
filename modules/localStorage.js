9b2a9be60dafbdc7ed95f812ccdda0852eb91c8a
Abrar052 <bc160400820@vu.edu.pk>
2022-08-16 10:54:50 +0500

    Created modules directory to add js modules

---
+++ modules/localStorage.js
@@ -0,0 +1,13 @@
+const storageAvailable = () => {
+  let storage;
+  try {
+    storage = window.localStorage;
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException;
+  }
+};
+export default storageAvailable;
